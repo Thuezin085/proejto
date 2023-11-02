dc5474cf87b3596442d5412318e2dd0775e23b01
Thuezin085 <araujogabarthur@gmail.com>
2023-11-02 11:01:07 -0300

    PJ 01 : Pagina Cadastro

--- view/Auth/cadastro.php
+++ view/Auth/cadastro.php
@@ -6,7 +6,6 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-
         <!-- Font -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -14,7 +13,8 @@
 
         <!-- Css -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <link rel="stylesheet"  href ="../../public/css/cadastro.css.">
+        <link rel="stylesheet"  href ="../../public/css/navbar.css">
+        <link rel="stylesheet"  href ="../../public/css/cadastro.css">
 
 
         <meta charset="UTF-8">
@@ -22,9 +22,14 @@
         <title>Cadastro</title>
     </head>
 <body>
+    <?php include_once('../components/navbar.php') ?>
+
+    <form class="container mt-5">
+
+    
     
 
-
-
+        
+    </form>
 </body>
 </html>
